ed121adb06db14709165d7fab6eba85f063394d9
OlatideJoseph <olatidejoseph17@gmail.com>
2025-07-13 20:11:21 +0100

    Sign In Option

--- jumia/utils.py
+++ jumia/utils.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.firefox.webdriver import WebDriver as FireFoxWebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 WEBDRIVERS = WebDriver | FireFoxWebDriver
@@ -25,3 +27,19 @@ def find_and_close_popup(driver: WEBDRIVERS, **kwargs):
                 close_button.click()
         except NoSuchElementException:
             return None
+
+
+def sign_in(driver: WEBDRIVERS, **kwargs):
+    """Signs In To The Application"""
+    wait = WebDriverWait(driver, 20)
+    account = wait.until(
+        EC.presence_of_element_located((By.XPATH, "//label[@for='dpdw-login']"))
+    )
+    if account:
+        signin = wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//a[starts-with(@href, '/customer/account/login')]")
+            )
+        )
+        if signin:
+            signin.click()
