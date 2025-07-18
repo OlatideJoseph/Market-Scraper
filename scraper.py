ae77420517823ec42d23197ca89ae6962e18f450
OlatideJoseph <olatidejoseph17@gmail.com>
2025-07-18 21:26:02 +0100

    New Changes

--- scraper.py
+++ scraper.py
@@ -1,15 +1,15 @@
 import time
-import re
 import csv
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webelement import WebElement
+
 
 from jumia.descriptor import PAGE_DESCRIPTION, JumiaContent
-from jumia.utils import find_and_close_popup
+from jumia.utils import find_and_close_popup, sign_in
 
+from utils import attribute_matches
 
 def create_firefox_driver(headless=False):
     """Creates A Driver For Firefox"""
@@ -44,24 +44,6 @@ def create_chrome_driver(headless=False):
     return driver
 
 
-def element_attribute_matches(
-    element: WebElement, attr: str = "href", reg_exp: str = r"*"
-):
-    return bool(re.match(reg_exp, element.get_attribute(attr)))
-
-
-def attribute_matches(
-    attr: str = "href", reg_exp: str = r"*", elements: list[WebElement] = []
-):
-    """
-    Returns The Elements That matches The Specified Attributes And Pattern
-    """
-
-    return [
-        element
-        for element in elements
-        if element_attribute_matches(element, attr, reg_exp)
-    ]
 
 
 def get_page_links(driver, link_description=PAGE_DESCRIPTION):
@@ -70,13 +52,14 @@ def get_page_links(driver, link_description=PAGE_DESCRIPTION):
     )
 
 
-driver = create_chrome_driver(headless=True)
+driver = create_chrome_driver(headless=False)
 
 driver.get("https://www.jumia.com.ng/")
 
-time.sleep(5)
+
 
 find_and_close_popup(driver)
+sign_in(driver)
 
 footer = driver.find_element(By.TAG_NAME, "footer")
 
@@ -90,6 +73,8 @@ anchors_with_matched_links = get_page_links(driver=driver)
 #     print(e, e.get_attribute("href"))
 
 jumia_contents = []
+
+
 
 
 def get_pages_contents(
@@ -108,14 +93,16 @@ def get_pages_contents(
     return storage
 
 
-storage = get_pages_contents(6)
 
-with open("filename.csv", "w+") as f:
-    fieldnames = ["title", "price", "details", "specifications"]
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writeheader()
-    for i in storage:
-        print(writer.writerow(i.as_dict()))
+
+# storage = get_pages_contents(6)
+
+# with open("filename.csv", "w+") as f:
+#     fieldnames = ["title", "price", "details", "specifications"]
+#     writer = csv.DictWriter(f, fieldnames=fieldnames)
+#     writer.writeheader()
+#     for i in storage:
+#         print(writer.writerow(i.as_dict()))
 
 # search = driver.find_element(By.NAME, "q")
 # search.clear()
@@ -128,4 +115,5 @@ time.sleep(20)
 # print(driver.page_source)
 
 if __name__ == "__main__":
-    driver.quit()
+    # driver.quit()
+    pass
